820385f1607379f11b9a6d024190fb7d4eec4c3e
Istiyak H. Siddiquee <istiyaksiddiquee@gmail.com>
2023-11-03 12:57:42 +0100

    snowballing

--- dags/ingestion_script.py
+++ dags/ingestion_script.py
@@ -1,16 +1,13 @@
-import pyarrow.parquet as pq
-import pandas as pd
 from sqlalchemy import create_engine
 
 
-def ingest_callable(user, password, host, port, db, table_name, parquet_file):
+def ingest_callable(user, password, host, port, db, table_name, df):
     
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     engine.connect()
 
     print('connection established successfully, inserting data...')
 
-    df = pd.read_parquet(parquet_file,  engine='pyarrow')
     df.store_and_fwd_flag.fillna('-999', inplace=True)
     df.passenger_count.fillna(-999, inplace=True)
     df.payment_type.fillna(-999, inplace=True)
--- dags/taskflow_trial.py
+++ dags/taskflow_trial.py
@@ -1,19 +1,15 @@
 import json
 import pandas as pd
 from airflow import DAG
-from numpy import datetime64
 from datetime import datetime
-import great_expectations as gx
-from urllib.error import HTTPError
 from airflow.decorators import task
-from pandera.errors import SchemaError
-from pandera import Column, DataFrameSchema, Float64, Index
-# from gx.initial_gx.plugins.expectations.expect_column_values_to_be_non_negative import ExpectColumnValuesToBeNonNegative
 
 
 @task
 def scrape_data_from_source(year_month_duo: str):
-    
+    import pandas as pd
+    from urllib.error import HTTPError
+
     URL_PREFIX = "https://d37ci6vzurychx.cloudfront.net/trip-data"
     URL_TEMPLATE = URL_PREFIX + "/green_tripdata_" + year_month_duo + ".parquet"
 
@@ -22,69 +18,89 @@ def scrape_data_from_source(year_month_duo: str):
         df = pd.read_parquet(URL_TEMPLATE)
     except HTTPError:
         print("the requested file is not available")
+        return None
 
     return df
 
 
-# def data_validation(df: pd.DataFrame) -> pd.DataFrame:
+@task
+def data_validation(df: pd.DataFrame):
 
-#     if df == None:
-#         return None
+    import pandas as pd
+    from numpy import datetime64
+    import great_expectations as gx
+    from pandera.errors import SchemaError
+    from pandera import Column, DataFrameSchema, Float64, Index
+    from expect_column_values_to_be_non_negative import ExpectColumnValuesToBeNonNegative
 
-#     schema = DataFrameSchema(
-#         {
-#             "VendorID": Column(Float64),
-#             "lpep_pickup_datetime": Column(datetime64),
-#             "lpep_dropoff_datetime": Column(datetime64),
-#             "store_and_fwd_flag": Column(object, nullable=True),
-#             "RatecodeID": Column(Float64, nullable=True),
-#             "PULocationID": Column(Float64),
-#             "DOLocationID": Column(Float64),
-#             "passenger_count": Column(Float64, nullable=True),
-#             "trip_distance": Column(Float64),
-#             "fare_amount": Column(Float64),
-#             "extra": Column(Float64),
-#             "mta_tax": Column(Float64),
-#             "tip_amount": Column(Float64),
-#             "tolls_amount": Column(Float64),
-#             "ehail_fee": Column(Float64, nullable=True),
-#             "improvement_surcharge": Column(Float64),
-#             "total_amount": Column(Float64),
-#             "payment_type": Column(Float64, nullable=True),
-#             "trip_type": Column(Float64, nullable=True),
-#             "congestion_surcharge": Column(Float64, nullable=True),
-#         },
-#         index=Index(int),
-#         strict=True,
-#     )
+    schema = DataFrameSchema(
+        {
+            "VendorID": Column(Float64, coerce=True),
+            "lpep_pickup_datetime": Column(datetime64),
+            "lpep_dropoff_datetime": Column(datetime64),
+            "store_and_fwd_flag": Column(object, nullable=True),
+            "RatecodeID": Column(Float64, nullable=True, coerce=True),
+            "PULocationID": Column(Float64, coerce=True),
+            "DOLocationID": Column(Float64, coerce=True),
+            "passenger_count": Column(Float64, coerce=True, nullable=True),
+            "trip_distance": Column(Float64, coerce=True),
+            "fare_amount": Column(Float64, coerce=True),
+            "extra": Column(Float64, coerce=True),
+            "mta_tax": Column(Float64, coerce=True),
+            "tip_amount": Column(Float64, coerce=True),
+            "tolls_amount": Column(Float64, coerce=True),
+            "ehail_fee": Column(Float64, coerce=True, nullable=True),
+            "improvement_surcharge": Column(Float64, coerce=True),
+            "total_amount": Column(Float64, coerce=True),
+            "payment_type": Column(Float64, coerce=True, nullable=True),
+            "trip_type": Column(Float64, coerce=True, nullable=True),
+            "congestion_surcharge": Column(Float64, coerce=True, nullable=True),
+        },
+        index=Index(int),
+        strict=True,
+    )
 
-#     try:
-#         schema.validate(df)
-#     except SchemaError:
-#         print("schema error")
-#         return None
-
-#     df['lpep_pickup_datetime'] = df['lpep_pickup_datetime'].astype('str')
-#     df['lpep_dropoff_datetime'] = df['lpep_dropoff_datetime'].astype('str')
     
-#     context = gx.data_context.DataContext('./gx/initial_gx')
-#     my_asset = context.get_datasource("my_taxi_validator_checkpoint").get_asset("taxi_frame")
-#     batch_request = my_asset.build_batch_request(dataframe=df)
-#     checkpoint = context.get_checkpoint(name="my_checkpoint")
-#     checkpoint_results = checkpoint.run_with_runtime_args(
-#         batch_request=batch_request,
-#         expectation_suite_name="my_expectation_suite"
-#     )
-#     if checkpoint_results.success != True:
-#         return None
+    try:
+        schema.validate(df)
+    except SchemaError:
+        print("schema error")
+        return None
+
+    df['lpep_pickup_datetime'] = df['lpep_pickup_datetime'].astype('str')
+    df['lpep_dropoff_datetime'] = df['lpep_dropoff_datetime'].astype('str')
+
+    context = gx.data_context.DataContext('./gx/initial_gx')
+    taxi_asset = context.get_datasource("green_taxi_validator_checkpoint").get_asset("taxi_df")
+    batch_request = taxi_asset.build_batch_request(dataframe=df)
+    checkpoint = context.get_checkpoint(name="green_taxi_checkpoint")
+    checkpoint_results = checkpoint.run_with_runtime_args(
+        batch_request=batch_request,
+        expectation_suite_name="green_taxi_expectation_suite"
+    )
+    print(checkpoint_results.success)
+    if checkpoint_results.success != True:
+        return None
+
+    return df
+
+@task
+def db_injection(year_month_duo: str, df: pd.DataFrame):
     
-#     return df
-
-
-# def db_injection(df: pd.DataFrame):
-#     # we have a df that we want to inject in our pg database
-#     # return successful if successful
-#     pass
+    # we have a df that we want to inject in our pg database
+    # return successful if successful
+    import os
+    from ingestion_script import ingest_callable
+    
+    PG_HOST = os.getenv('PG_HOST')
+    PG_USER = os.getenv('PG_USER')
+    PG_PASSWORD = os.getenv('PG_PASSWORD')
+    PG_PORT = os.getenv('PG_PORT')
+    PG_DATABASE = os.getenv('PG_DATABASE')
+    TABLE_NAME_TEMPLATE = 'green_taxi_' + year_month_duo
+    
+    if df.shape != 0:
+        ingest_callable(PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DATABASE, TABLE_NAME_TEMPLATE, df)
 
 
 # def process_bronze_layer():
@@ -137,23 +153,24 @@ def transform(raw_data: str):
 
 
 @task
-def load(largest_city):
+def load(df: pd.DataFrame):
     """
     Prints the name of the largest city in
     the set as determined by the transform.
     """
-    print(largest_city)
+
+    print(df.shape)
 
 
 with DAG(
     dag_id="green_taxi",
     schedule_interval="0 6 2 * *",
     start_date=datetime(2023, 1, 1),
-    end_date=datetime(2023, 5, 15)
+    end_date=datetime(2023, 3, 15),
 ) as dag:
     year_month_duo = "{{ execution_date.strftime('%Y-%m') }}"
-    # df = scrape_data_from_source(year_month_duo)
-    # print('done: ' + year_month_duo)
-    extracted_data = extract(year_month_duo)
-    largest_city = transform(extracted_data)
-    load(largest_city)
+    extracted_data = scrape_data_from_source(year_month_duo)
+    if extracted_data != None:
+        validated_data = data_validation(extracted_data)
+        if validated_data != None:
+            db_injection(year_month_duo, validated_data)
