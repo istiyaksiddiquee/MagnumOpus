d928101af3c08650ad89a060864d044580e42173
Istiyak H. Siddiquee <istiyaksiddiquee@gmail.com>
2026-09-20 12:48:17 +0200

    DBT bronze layer working, accumulated all the into a single table

--- dbt-transform/bronze/macros/list.sql
+++ dbt-transform/bronze/macros/list.sql
@@ -1,34 +1,12 @@
-{% macro get_table_names() %}
-
-{{ print("Running some_macro ") }}
-
 {% macro get_source_tables(source_name) %}
   {% set tables = [] %}
-  {% for node in graph.sources.values() %}
-    {% if node.source_name == source_name %}
-      {% do tables.append(node.name) %}
-    {% endif %}
-  {% endfor %}
+  {% if execute %}
+    {% for node in graph.sources.values() %}
+      {% if node.source_name == source_name %}
+        {% do tables.append(node.name) %}
+      {% endif %}
+    {% endfor %}
+  {% endif %}
   {{ return(tables) }}
 {% endmacro %}
-
-{% set schema_list_query %}
-    select 
-      table_name 
-    from 
-      psql.information_schema.tables 
-    where 
-      table_schema = 'public'
-{% endset %}
-
-{% set results = run_query(schema_list_query) %}
-{% if execute %}
-{# Return the first column #}
-{% set results_list = results.columns[0].values() %}
-{{ print("macro output: " ~ results_list) }}
-{{ return (results_list) }}
-{% else %}
-{% set results_list = 'nothing' %}
-{% endif %}
-
-{% endmacro %}
+ 
--- dbt-transform/bronze/models/accumulate.sql
+++ dbt-transform/bronze/models/accumulate.sql
@@ -1,31 +1,36 @@
 {{
     config(
         materialized = 'incremental',
-        incremental_strategy='append'
+        incremental_strategy = 'append'
     )
 }}
 
-{% set tables = get_source_tables('lakehouse_db') %}
-{% for table in tables %}
+{% set tables = get_source_tables('datalake') %}
+
+{% for t in tables %}
 select
-  cast (vendorid as BIGINT) as vendorid, 
-  cast (ratecodeid as BIGINT) as ratecodeid,
-  cast (pulocationid as BIGINT) as pickup_locationid,
-  cast (dolocationid as BIGINT) as dropoff_locationid,
+  cast (vendor_id as BIGINT) as vendor_id,
   cast (lpep_pickup_datetime AS TIMESTAMP(6)) as pickup_datetime,
   cast (lpep_dropoff_datetime AS TIMESTAMP(6)) as dropoff_datetime,
-  store_and_fwd_flag,
+  cast (rate_code_id as BIGINT) as rate_code_id,
+  cast (pu_location_id as BIGINT) as pickup_locationid,
+  cast (do_location_id as BIGINT) as dropoff_locationid,
   cast (passenger_count as BIGINT) as passenger_count,
   cast (trip_distance as DOUBLE) as trip_distance,
   cast (trip_type as BIGINT) as trip_type,
   cast (fare_amount as DOUBLE) as fare_amount,
   cast (extra as DOUBLE) as extra,
-  cast(mta_tax as DOUBLE) as mta_tax,
+  cast (mta_tax as DOUBLE) as mta_tax,
   cast (tip_amount as DOUBLE) as tip_amount,
   cast (tolls_amount as DOUBLE) as tolls_amount,
   cast (ehail_fee as BIGINT) as ehail_fee,
   cast (improvement_surcharge as DOUBLE) as improvement_surcharge,
   cast (total_amount as DOUBLE) as total_amount,
   cast (payment_type as BIGINT) as payment_type,
-  cast (congestion_surcharge as DOUBLE) as congestion_surcharge
-from {{ source('postgres', var('db_name')) }}
+  cast (congestion_surcharge as DOUBLE) as congestion_surcharge,
+  cast (cbd_congestion_fee as DOUBLE) as cbd_congestion_fee
+from {{ source('datalake', t) }}
+{% if not loop.last %}
+union all
+{% endif %}
+{% endfor %}
